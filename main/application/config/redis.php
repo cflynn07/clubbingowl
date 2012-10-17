94f5be62c0d11c2164f82da1f30c504d622ae7ef
Casey <casey@Casey-Flynns-MacBook-Air.local>
2012-10-17 11:31:07 -0400

    test

--- main/application/config/redis.php
+++ main/application/config/redis.php
@@ -12,11 +12,11 @@ if(MODE == 'production' || MODE == 'staging'){
 	
 	
 	
-	//$config['redis_host'] 		= (isset($dotcloud['DOTCLOUD_DATA_REDIS_HOST'])) 		? $dotcloud['DOTCLOUD_DATA_REDIS_HOST'] 	: '';		// IP address or host
-	$config['redis_host'] 		= $dotcloud['DOTCLOUD_DATA_REDIS_LOGIN'] . ':' . $dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'] . '@' . $dotcloud['DOTCLOUD_DATA_REDIS_HOST'];
+	$config['redis_host'] 		= (isset($dotcloud['DOTCLOUD_DATA_REDIS_HOST'])) 		? $dotcloud['DOTCLOUD_DATA_REDIS_HOST'] 	: '';		// IP address or host
+//	$config['redis_host'] 		= $dotcloud['DOTCLOUD_DATA_REDIS_LOGIN'] . ':' . $dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'] . '@' . $dotcloud['DOTCLOUD_DATA_REDIS_HOST'];
 	$config['redis_port'] 		= (isset($dotcloud['DOTCLOUD_DATA_REDIS_PORT'])) 		? $dotcloud['DOTCLOUD_DATA_REDIS_PORT'] 	: '';		// Default Redis port is 6379
-//	$config['redis_password'] 	= (isset($dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'])) 	? $dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'] : '';		// Can be left empty when the server does not require AUTH
-	$config['redis_password'] 	= '';
+	$config['redis_password'] 	= (isset($dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'])) 	? $dotcloud['DOTCLOUD_DATA_REDIS_PASSWORD'] : '';		// Can be left empty when the server does not require AUTH
+//	$config['redis_password'] 	= '';
 	
 }else{
 	
