3574647d2eac0ad1e017e19ff3b8e89c1cfa7def
Casey <casey@Casey-Flynns-MacBook-Air.local>
2012-10-23 22:27:14 -0400

    fix

--- main/application/views/dynamic_assets/ejs/primary_no_friends.php
+++ main/application/views/dynamic_assets/ejs/primary_no_friends.php
@@ -1,5 +1,5 @@
 <div id="no_friends">
-	<img id="toro" src="<%= window.module.Globals.prototype.front_assets + 'images/clubbingOwlBackgroundWeb.png' %>" />
+	<img id="toro" src="<%= window.module.Globals.prototype.front_assets + 'images/ClubbingOwlBackgroundWeb.png' %>" />
 	<div id="message">
 		<p><strong>Congratulations!</strong> Looks like you are the first in your friend-circle to discover ClubbingOwl! Join guest-lists and reserve tables at your favorite <a style="color:#474D6A" class="nav_link" href="<%= window.module.Globals.prototype.front_link_base %>venues/">venues</a>. Or make your visit a social experience and invite the rest of your <a style="color:#474D6A" class="nav_link" href="<%= window.module.Globals.prototype.front_link_base %>friends/">friends</a>!</p>
 	</div>
