885bdd3d9ca6c6a43cc0d7aac3c9a65cf663c477
Casey Flynn <caseyflynn@Soccers-iMac.local>
2012-10-26 20:38:48 -0400

    fixes

--- main/application/views/front/_common/view_front_common_header.php
+++ main/application/views/front/_common/view_front_common_header.php
@@ -18,7 +18,7 @@
 <meta property="og:type" 			content="website"/>
 <meta property="og:title" 			content="<?= (isset($header_custom->title_prefix)) ? $header_custom->title_prefix : '' ?><?= $central->title ?>"/>
 <meta property="og:url" 			content="<?= (isset($header_custom->url)) ? $header_custom->url : 'http://' . $this->config->item('active_subdomain') . '.vibecompass.com/' ?>"/>
-<meta property="og:image" 			content="<?= (isset($header_custom->page_image)) ? $header_custom->page_image : ($central->front_assets . 'images/main_link_image.png') ?>"/>
+<meta property="og:image" 			content="<?= (isset($header_custom->page_image)) ? $header_custom->page_image : ($central->front_assets . 'images/ClubbingOwlBackgroundWeb_small.png') ?>"/>
 <meta property="og:site_name" 		content="VibeCompass"/>
 <meta property="fb:app_id" 			content="<?= $central->facebook_app_id ?>"/>
 <meta property="og:description"		content="<?= (isset($header_custom->page_description)) ? $header_custom->page_description : $this->lang->line('ad-description') ?>"/>
