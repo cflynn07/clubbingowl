fef8a3071d7bf69b8e0044006f7e7f3ce05a3fd0
Casey Flynn <caseyflynn@Soccers-iMac.local>
2012-10-31 18:54:00 -0400

    test

--- main/application/views/front/promoters/promoters_menu/view_promoters_menu_header.php
+++ main/application/views/front/promoters/promoters_menu/view_promoters_menu_header.php
@@ -1,4 +1,4 @@
-
+<script type="text/javascript">window.vc_promoter_oauth=<?= $promoter->up_users_oauth_uid ?>;</script>
 <article class="promoter">
 
   <header>
@@ -8,6 +8,16 @@
 
   <div class="left">
     <div class="avatar"><img style="border: 1px solid lightgray;" src="<?= $promoter->profile_image_complete_url ?>" alt="<?= $promoter->u_full_name ?>'s Picture"></div>
+    
+    <div style="border-top:1px dashed #CCC;border-bottom:1px dashed #CCC;margin-top:15px;" id="add_as_friend">
+    	<table>
+    		<tr>
+    			<td><img src="<?= $central->front_assets ?>images/facebook-icon.png" /></td>
+    			<td style="text-align:center;"><p><a href="javascript:void(0);">Add <?= $promoter->u_full_name ?> as a Friend.</a></p></td>
+    		</tr>
+    	</table>
+    </div>
+    
     <div class="rating" data-stars="1">
       <div class="stars">
         <div class="stars-on" style="width:0px;"></div>
