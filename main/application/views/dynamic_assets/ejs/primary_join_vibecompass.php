d24712b71910d9cd7e897c0f36860c0c65bfe9e4
Casey <casey@Casey-Flynns-MacBook-Air.local>
2012-11-06 21:13:30 -0500

    latest

--- main/application/views/dynamic_assets/ejs/primary_join_vibecompass.php
+++ main/application/views/dynamic_assets/ejs/primary_join_vibecompass.php
@@ -2,7 +2,7 @@
 	
 	<table style="width:100%;">
 		<tr>
-			<td style="vertical-align:top; max-width:50px;">
+			<td style="vertical-align:top; width:50px;">
 				
 				<div class="avatar">
 					<%
