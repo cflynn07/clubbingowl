ddd4b9fb075c59187bc4071d68c6aa86a1252af3
Casey Flynn <casey_flynn@cobarsystems.com>
2012-11-17 10:37:18 -0500

    added ability to fully collapse left-hand menu

--- main/vcweb2/assets/admin/js/promoters/admin_promoters_backbone_view_wrapper.js
+++ main/vcweb2/assets/admin/js/promoters/admin_promoters_backbone_view_wrapper.js
@@ -40,11 +40,28 @@ jQuery(function(){
 		events_click_menu_item: function(e){
 					
 			var el = jQuery(e.currentTarget);
-			el.parent().find('ul').slideUp('fast');
-			el.parent().find("li").removeClass("current");
-			el.find("ul").slideToggle('fast');
-			el.toggleClass("current");
-			
+			if(el.hasClass('current')){
+				if(el.find('ul')){
+					el.find('ul').slideToggle('fast');
+				}
+				return;
+			}else{
+				
+				if(el.find('ul')){
+					
+					this.$el.find('#primary_left .current').removeClass('current');
+					el.toggleClass('current');
+					el.find('ul').slideToggle('fast');
+					
+				}else{
+					
+					
+				//	el.find('ul').slideToggle('fast');
+				
+				}
+				
+			}
+						
 		},
 		events_hover_notification: function(e){
 			
