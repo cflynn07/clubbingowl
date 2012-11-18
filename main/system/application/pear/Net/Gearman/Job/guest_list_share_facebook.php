a5b3c11f6b57b476fb533f50d1dab674bedae1fc
Casey <casey@Casey-Flynns-MacBook-Air.local>
2012-11-18 09:35:30 -0500

    updated wall-post message to be less dumb.

--- main/system/application/pear/Net/Gearman/Job/guest_list_share_facebook.php
+++ main/system/application/pear/Net/Gearman/Job/guest_list_share_facebook.php
@@ -35,11 +35,12 @@ class Net_Gearman_Job_guest_list_share_facebook extends Net_Gearman_Job_Common{
 			$team_venue_id		= $args['team_venue_id'];
 			
 			$params = array(
-				'message' 		=> "$vc_user->users_full_name is on the ClubbingOwl guest list '$guest_list_name' at $venue_name " . ((date('l', strtotime($date)) == date('l', time())) ? 'today' : date('l', strtotime($date))) . "!",
+//				'message' 		=> "$vc_user->users_full_name is on the ClubbingOwl guest list '$guest_list_name' at $venue_name " . ((date('l', strtotime($date)) == date('l', time())) ? 'today' : date('l', strtotime($date))) . "!",
+				'message' 		=> "I'm on the guest list '$guest_list_name' at $venue_name " . ((date('l', strtotime($date)) == date('l', time())) ? 'today' : date('l', strtotime($date))) . "! Click here to join \"$guest_list_name\"",
 				'link' 			=> "www.facebook.com/pages/@/$team_venue_id?sk=app_$facebook_application_id",
 				'picture' 		=> ($image) ? '' : $CI->config->item('global_assets') . 'images/ClubbingOwlBackgroundWeb_small2.png',
 				'name' 			=> $guest_list_name,
-				'caption' 		=> "Click here to Join '$guest_list_name'",
+				'caption' 		=> "Click here to join \"$guest_list_name\"",
 				'description' 	=> $app_description
 			);
 			
@@ -60,11 +61,11 @@ class Net_Gearman_Job_guest_list_share_facebook extends Net_Gearman_Job_Common{
 				$base_url = 'http://www.clubbingowl.com/';
 				
 			$params = array(
-				'message' 		=> "$vc_user->users_full_name is on $promoter_full_name's guest list '$guest_list_name' at $venue_name " . ((date('l', strtotime($date)) == date('l', time())) ? 'today' : date('l', strtotime($date))) . "!",
-				'link' 			=> $base_url . "promoters/boston/$promoter_public_identifier/guest_lists/$guest_list_url_name/?ref=$user_third_party_id",
+				'message' 		=> "I'm on $promoter_full_name's guest list \"$guest_list_name\" at $venue_name " . ((date('l', strtotime($date)) == date('l', time())) ? 'today' : date('l', strtotime($date))) . "! Click here to join\"$guest_list_name\"",
+				'link' 			=> $base_url . "promoters/$promoter_public_identifier/guest_lists/$guest_list_url_name/?ref=$user_third_party_id",
 				'picture' 		=> ($image) ? $CI->config->item('s3_uploaded_images_base_url') . 'guest_lists/' . $image . '_t.jpg' : $CI->config->item('global_assets') . 'images/vibecompass_logo.png',
 				'name' 			=> $guest_list_name,
-				'caption' 		=> "Click here to Join '$guest_list_name'",
+				'caption' 		=> "Click here to join \"$guest_list_name\"",
 				'description' 	=> $app_description
 			);
 			
