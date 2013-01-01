6c436124fc3d800ad77ee5b035819ee67a60757d
Casey Flynn <casey_flynn@cobarsystems.com>
2012-12-31 21:29:38 -0500

    tweaking UI of client-notes page in promoter admin panel

--- main/application/views/admin/promoters/clients/view_clients_individual.php
+++ main/application/views/admin/promoters/clients/view_clients_individual.php
@@ -10,61 +10,89 @@
 
 <div id="clients_individual_wrapper" class="full_width last">
 	
-	<h1 data-client-name=""></h1>
-	
 	<div class="ui-widget">
 		
-		<div style="min-height:200px;" class="one_fifth">
-			<img style="border:1px solid #CCC;" id="client_pic" />
+		<div class="ui-widget-header">
+			<span data-client-name=""></span>
 		</div>
-		<div class="four_fifth last">
-			<h2>Info</h2>
+		
+		<div style="width:100%;" class="">
 			
-			<table class="normal" style="width:100%;">
-				<tbody>
-					<tr>
-						<td class="key">Phone Number:</td>
-						<td data-phone-number=""></td>
-					</tr>
-					<tr>
-						<td class="key">Email:</td>
-						<td data-email=""></td>
-					</tr>
-					
-					<?php if($client === false): ?>
-						
+			<div style="min-height:200px;" class="one_fifth">
+				<img style="border:1px solid #CCC;" id="client_pic" />
+			</div>
+			<div class="four_fifth last">
+				
+				<div class="ui-widget-header">
+					<span>Client Information</span>
+				</div>
+				
+				<table class="normal" style="width:100%;">
+					<tbody>
 						<tr>
-							<td colspan="2"><p style="color:red; margin:0;"><span data-client-name=""></span> has not joined one of your guest-lists. You will not be able to see his/her email address and phone number until then.</p></td>
+							<td class="key">Phone Number:</td>
+							<td data-phone-number=""></td>
+						</tr>
+						<tr>
+							<td class="key">Email:</td>
+							<td data-email=""></td>
 						</tr>
 						
-					<?php endif; ?>
-					
-				</tbody>
-			</table>
+						<?php if($client === false): ?>
+							
+							<tr>
+								<td colspan="2"><p style="color:red; margin:0;"><span data-client-name=""></span> has not joined one of your guest-lists. You will not be able to see his/her email address and phone number until then.</p></td>
+							</tr>
+							
+						<?php endif; ?>
+						
+					</tbody>
+				</table>
+			</div>
 			
 		</div>
 		
-		<div class="full_width last" style="clear:both;" >
-			<h2>My Private Notes</h2>
-			<p class="subnote">These notes are private to you and will not be shared with your manager or other members of your team.</p>
-			<textarea id="private_notes" class="notes"><?= ($data->my_client_notes) ? $data->my_client_notes->private_notes : '' ?></textarea>
-		</div>
-		<a href="#" style="float:right; margin-right:13px;" data-action="save" class="button_link btn-action">Save</a>	
-		<img style="display:none; float:right; margin-right:13px;" class="loading_indicator" src="<?= $central->global_assets . 'images/ajax.gif' ?>" alt="loading..." />
-		
-		<div class="full_width last" style="clear:both;">
-			<h2>My Public Notes</h2>
-			<p class="subnote">These notes are public. Share information about this client with your managers and other promoters on your team.</p>
-			<textarea id="public_notes" class="notes"><?= ($data->my_client_notes) ? $data->my_client_notes->public_notes : '' ?></textarea>
-		</div>
-		<a href="#" style="float:right; margin-right:13px;" data-action="save" class="button_link btn-action">Save</a>	
-		<img style="display:none; float:right; margin-right:13px;" class="loading_indicator" src="<?= $central->global_assets . 'images/ajax.gif' ?>" alt="loading..." />
-		
-		<hr>
-		
-		<div>
+		<hr style="border:1px solid #CCC; "/>
+				
+				
+				
+		<div style="width:45%; float:left;" class="ui-widget one_half">
 			
-			<h1>Team Notes on <span data-client-name=""></span></h1>
+			<div class="ui-widget-header">
+				<span>My Notes</span>
+			</div>
+			
+			<div style="clear:both;" >
+				<h2 style="color:#000; text-decoration:underline;">My Private Notes</h2>
+				<p class="subnote">These notes are private to you and will not be shared with your manager or other members of your team.</p>
+				<textarea id="private_notes" class="notes"><?= ($data->my_client_notes) ? $data->my_client_notes->private_notes : '' ?></textarea>
+			</div>
+			<a href="#" style="float:right; margin-right:13px;" data-action="save" class="button_link btn-action">Save</a>	
+			<img style="display:none; float:right; margin-right:13px;" class="loading_indicator" src="<?= $central->global_assets . 'images/ajax.gif' ?>" alt="loading..." />
+			
+			
+			
+			
+			<div style="clear:both;">
+				<h2 style="color:#000; text-decoration:underline;">My Public Notes</h2>
+				<p class="subnote">These notes are public. Share information about this client with your managers and other promoters on your team.</p>
+				<textarea id="public_notes" class="notes"><?= ($data->my_client_notes) ? $data->my_client_notes->public_notes : '' ?></textarea>
+			</div>
+			<a href="#" style="float:right; margin-right:13px;" data-action="save" class="button_link btn-action">Save</a>	
+			<img style="display:none; float:right; margin-right:13px;" class="loading_indicator" src="<?= $central->global_assets . 'images/ajax.gif' ?>" alt="loading..." />
+	
+	
+	
+		</div>
+				
+		
+		
+		
+		<div style="width:45%; float:right;" class="ui-widget one_half last">
+			
+			<div class="ui-widget-header">
+				<span>Team Notes</span>
+			</div>
 			
 			<?php if(!$data->client_notes_team): ?>
 				
@@ -93,6 +121,10 @@
 			
 		</div>
 
+
+
+
+
 		<?php if(false): ?>
 		
 		<div class="full_width last" style="clear:both;">
@@ -100,7 +132,9 @@
 		</div>
 		
 		<?php endif; ?>
-		
-	</div>
 	
+	
+	<div style="clear:both;"></div>	
+	</div>
+		
 </div>
