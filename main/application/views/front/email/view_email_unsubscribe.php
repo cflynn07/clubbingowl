70efa0c46243283df6ef17bb3e177aebc67d7d9b
Casey Flynn <casey_flynn@cobarsystems.com>
2013-01-04 05:41:28 -0500

    ui-tweak

--- main/application/views/front/email/view_email_unsubscribe.php
+++ main/application/views/front/email/view_email_unsubscribe.php
@@ -1,7 +1,7 @@
 <div id="error_wrapper">
 	<div style="width:100%;" id="message">
 		
-		<p style="width:80%; margin-left:auto; margin-right:auto; text-align:left;">Hi <?= $user->first_name ?>,<img src="https://graph.facebook.com/<?= $user->oauth_uid ?>/picture?type=large" style="float:right; border-radius:10px; border:1px solid #CCC;"/><br/><br/>You have been unsubscribed from all ClubbingOwl promotional emails.<br/><br/>If you want to change your email settings, just log in and click on 'profile' in the top menu.</p>
+		<p style="width:80%; margin-left:auto; margin-right:auto; text-align:left;">Hey <?= $user->first_name ?><img src="https://graph.facebook.com/<?= $user->oauth_uid ?>/picture?type=large" style="float:right; border-radius:10px; border:1px solid #CCC;"/><br/><br/>You have been unsubscribed from all ClubbingOwl promotional emails.<br/><br/>If you want to change your email settings, just log in and click on 'profile' in the top menu.</p>
 		
 	
 		
