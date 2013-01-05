28598b2e7707fcb96798170bc1871f2adc6be28b
Casey Flynn <casey_flynn@cobarsystems.com>
2013-01-05 05:24:35 -0500

    fix

--- main/application/views/dynamic_assets/ejs/friends_nonvc_friend.php
+++ main/application/views/dynamic_assets/ejs/friends_nonvc_friend.php
@@ -1,6 +1,6 @@
 <li>
     <div class="avatar">
-    	<%=image_insert(pic_square, {alt: name + '\'s Avatar', class: 'venue-image'})%>
+    	<%=image_insert(pic_square, {alt: name + '\'s Avatar', class: ''})%>
     </div>
     <span class="name"><%= name %></span>
     <a data-uid="<%= uid %>" href="javascript: void(0);" class="invite">Invite</a>
--- main/application/views/dynamic_assets/ejs/friends_vc_friend.php
+++ main/application/views/dynamic_assets/ejs/friends_vc_friend.php
@@ -1,6 +1,6 @@
 <li>
     <div class="avatar">
-    	<%= inline_link('friends/' + third_party_id, image_insert(pic_square, {alt: name + '\'s Avatar'}), {class: 'venue-image'}) %>
+    	<%= inline_link('friends/' + third_party_id, image_insert(pic_square, {alt: name + '\'s Avatar'}), {class: ''}) %>
     </div>
     <%= inline_link('friends/' + third_party_id, name, {class: 'name'}) %>
 </li>
