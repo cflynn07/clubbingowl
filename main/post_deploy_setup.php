bcd30f068c381fcd273c8a4ea52051d4a5eb5338
Casey Flynn <casey_flynn@cobarsystems.com>
2013-01-10 08:56:35 -0500

    test

--- main/post_deploy_setup.php
+++ main/post_deploy_setup.php
@@ -3,11 +3,18 @@
 if(php_sapi_name() != 'cli')
 	die();
 
+
+// Read the file and convert the underlying JSON dictionary into a PHP array
+$env = json_decode(file_get_contents("/home/dotcloud/environment.json"), true);
+	 
+
 $custom_env_file = '/home/dotcloud/current/custom.json';
 $fh = fopen($custom_env_file, 'w+');
 
 $json = new stdClass;
-$json->deployment_unique_id = md5(time() . rand(0, 1000));
+//$json->deployment_unique_id = md5(time() . rand(0, 1000));
+
+$json->deployment_unique_id = $env['DEPLOYMENT_UNIQUE_ID'];
 
 fwrite($fh, json_encode($json));
 fclose($fh);
